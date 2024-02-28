e2a8d28b6c27c8f4a8741b9ddc88d87efb3a08b1
이성종 <104560357+LSJTHON@users.noreply.github.com>
2024-02-28 17:03:59 +0900

    Add files via upload

--- WebMarket/src/main/java/filter/LogFileFilter.java
+++ WebMarket/src/main/java/filter/LogFileFilter.java
@@ -22,7 +22,42 @@ public class LogFileFilter implements Filter{
 			writer = new PrintWriter(new FileWriter(filename, true), true);
 			
 		}catch(IOException e) {
-			
+			throw new ServletException("로그 파일을 열 수 없습니다.");
 		}
 	}
+	
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws java.io.IOException, ServletException{
+		writer.println(" 접속한 클라이언트 IP : "+request.getRemoteAddr());
+		long start = System.currentTimeMillis();
+		writer.println(" 접근한 URL 경로 : "+getURLPath(request));
+		writer.println(" 요청 처리 시작 시각 :"+getCurrentTime());
+		
+		chain.doFilter(request, response);
+		
+		long end = System.currentTimeMillis();
+		writer.println("요청 처리 종료 시각 : "+getCurrentTime());
+		writer.println("요청 처리 소요 시간 : "+(end-start)+"ms ");
+	}
+	public void destroy() {
+		writer.close();
+	}
+	private String getURLPath(ServletRequest request) {
+		HttpServletRequest req;
+		String currentPath ="";
+		String queryString = "";
+		if(request instanceof HttpServletRequest) {
+			req=(HttpServletRequest) request;
+			currentPath = req.getRequestURI();
+			queryString = req.getQueryString();
+			queryString = queryString == null ? "" : "?" + queryString;
+		}
+		return currentPath + queryString;
+	}
+	private String getCurrentTime() {
+		DateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		return formatter.format(calendar.getTime());
+		}
 }
